701b2074045712a8e70111fe0db80cb6bea24cfc
gianlucastefanoni <gianlucastefanoni19@gmail.com>
2025-04-23 22:28:21 +0200

    fix static routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, HashRouter } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { PortfolioFullstack } from "./pages/PortfolioFullstack";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/portfolio" element={<Home />} />
         <Route path="/portfolio/fullstack" element={<PortfolioFullstack />} />
         <Route path="/" element={<Navigate to="/portfolio" replace />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
