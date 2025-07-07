ba3ca64f682d6b432af795854e6eebb497c0b9c7
gianlucastefanoni <gianlucastefanoni19@gmail.com>
2025-07-07 16:28:33 +0200

    fix home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,34 @@
 import { useNavigate } from "react-router-dom";
+import "../App.css";
+import userIcon1 from "../assets/user-icon-1.png";
+import userIcon2 from "../assets/user-icon-2.png";
 
 export const Home = () => {
   const navigate = useNavigate();
+
   return (
-    <div className="h-dvh w-dvw bg-black text-white !p-4 flex flex-col">
-      <div className="w-full text-5xl text-center font-bebas">
-        Stefanoni Gianluca
-      </div>
-      <div className="w-full h-2/3 flex justify-around items-center">
-        <div className="cursor-pointer" onClick={() => navigate("/fullstack")}>
-          Full Stack Developer
+    <div className="home-netflix-style">
+      <h1 className="main-title">Stefanoni Gianluca</h1>
+
+      <div className="profiles">
+        <div
+          className="profile-card"
+          onClick={() => navigate("/fullstack")}
+        >
+          <div className="avatar">
+            <img src={userIcon2} alt="Full Stack" />
+          </div>
+          <span>Full Stack</span>
         </div>
-        <div className="cursor-pointer" onClick={() => navigate("/gamedev")}>
-          Game Developer
+
+        <div
+          className="profile-card"
+          onClick={() => navigate("/gamedev")}
+        >
+          <div className="avatar">
+            <img src={userIcon1} alt="Game Dev" />
+          </div>
+          <span>Game Dev</span>
         </div>
       </div>
     </div>
